64a4be95e8c5b1f7ec6ea2cb8d5c1ce1d6284b85
Vyacheslav Buzivskyy <v.buzivskoy@gmail.com>
2022-01-18 16:39:21 +0200

    add tag mongoose model

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,11 @@
 const ArticleModel = require('./article');
 const CommentModel = require('./comment');
 const UserModel = require('./user');
+const TagModel = require('./tag');
 
 module.exports = {
   ArticleModel,
   CommentModel,
   UserModel,
+  TagModel,
 };
---
+++ src/models/tag.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const tagSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      index: true,
+      required: true,
+    },
+    articles: [{ type: 'ObjectId', ref: 'Article' }],
+  },
+);
+
+tagSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+tagSchema.set('toJSON', {
+  virtuals: true,
+});
+
+module.exports = model('Tag', tagSchema);
